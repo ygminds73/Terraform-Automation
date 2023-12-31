c35f5bbcca0703f0bb29d0a3b9d0ca568e196e0f
Young Minds <109673364+ygminds73@users.noreply.github.com>
2023-12-31 09:51:13 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "myJune2021Key" 
+    default     =  "batch-22" 
 }
 
 variable "instance_type" { 
