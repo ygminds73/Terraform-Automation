30a2da765eb41b1d9f5aa2b5c6ee5656c9f54756
BHUSHAN-GAJARE <gajare.bhushan46@gmail.com>
2024-01-16 22:06:18 +0530

    main/terraform.tfstate added in backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "mydev-tf-state-bucket-project-terraproject-01"
-    key = "main"
+    key = "main/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table_01"
   }
