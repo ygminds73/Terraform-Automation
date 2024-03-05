7024dc73b2d193b3c8f17221886d1bc3b39eac8d
Nikita-Clou <157437747+Nikita-Clou@users.noreply.github.com>
2024-03-05 19:57:40 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-project-terraform-batch-22"
+    bucket = "batch2-app-nikita"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
