2504d23ebebcbc1fd9b776e8f6f6e9c0094d970a
Shub23101998 <94104262+Shub23101998@users.noreply.github.com>
2024-07-12 18:45:48 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,10 +1,1 @@
-resource "aws_s3_bucket" "my-s3-bucket-17890" {
-  bucket_prefix = var.bucket_prefix
-  acl = var.acl
-  
-   versioning {
-    enabled = var.versioning
-  }
-  
-  tags = var.tags
-}
+
