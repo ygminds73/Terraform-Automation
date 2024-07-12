7ce1ba558240b4f0e2cc505b87df625c7d30dd0d
Shub23101998 <94104262+Shub23101998@users.noreply.github.com>
2024-07-12 18:48:05 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -36,10 +36,7 @@ variable "acl" {
     description = " Defaults to private "
     default     = "private"
 }
-variable "bucket_prefix" {
-    type        = string
-    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
-    default     = "my-s3-bucket-17890"
+
 }
 variable "tags" {
     type        = map
