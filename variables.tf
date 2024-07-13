4e7dbe020cec1c816c136b5541d08a3fbf2c867a
Shub23101998 <94104262+Shub23101998@users.noreply.github.com>
2024-07-14 01:32:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-07ffb2f4d65357b42" 
+    default     = "ami-04a81a99f5ec58529" 
 }
 variable "versioning" {
     type        = bool
