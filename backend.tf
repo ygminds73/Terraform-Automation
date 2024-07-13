69f2f06dae26118aad777ee000d74204c2526670
Shub23101998 <94104262+Shub23101998@users.noreply.github.com>
2024-07-14 00:02:18 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,8 @@
-
+terraform {
+  backend "s3" {
+    bucket = "mydev-project-terraform-batch-24-shubh-bucket-123"
+    key = "main"
+    region = "us-east-1"
+    dynamodb_table = "my-dynamodb-table"
+  }
+}
