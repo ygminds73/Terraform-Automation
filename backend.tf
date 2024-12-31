a3710dc83a91388b7d2fca164d1ca1a42d455606
Young Minds <109673364+ygminds73@users.noreply.github.com>
2024-12-31 08:11:40 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-project-terraform-batch-25"
+    bucket = "mydev-project-terraform-batch-26"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
