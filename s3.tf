7f929079c41ccda9d2d26079d7080d045f15aec2
Young Minds <109673364+ygminds73@users.noreply.github.com>
2025-09-20 10:53:43 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,10 +1,12 @@
 resource "aws_s3_bucket" "my-s3-bucket" {
   bucket_prefix = var.bucket_prefix
   acl = var.acl
-  
-   versioning {
-    enabled = var.versioning
-  }
-  
   tags = var.tags
+}
+
+resource "aws_s3_bucket_versioning" "my-s3-bucket-versioning" {
+  bucket = aws_s3_bucket.my-s3-bucket.id
+  versioning_configuration {
+    status = var.versioning ? "Enabled" : "Suspended"
+  }
 }
