ffaaa69da6f156ac29bc7cf8585b32f1638f27c2
Young Minds <109673364+ygminds73@users.noreply.github.com>
2025-09-20 10:56:24 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,7 +1,11 @@
 resource "aws_s3_bucket" "my-s3-bucket" {
   bucket_prefix = var.bucket_prefix
-  acl = var.acl
   tags = var.tags
+}
+
+resource "aws_s3_bucket_acl" "my-s3-bucket-acl" {
+  bucket = aws_s3_bucket.my-s3-bucket.id
+  acl    = var.acl
 }
 
 resource "aws_s3_bucket_versioning" "my-s3-bucket-versioning" {
