4e497a5b129668c0e809cd46fd49ac1b31bd4676
technicalguftgu <aniketshingade358@gmail.com>
2026-03-31 00:27:10 +0530

    name got changed

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "young-minds-app-project-terraform-state-latest"
+    bucket         = "tanuj bucket "
     key            = "env/dev/terraform.tfstate"
     region         = "us-east-1"
 
