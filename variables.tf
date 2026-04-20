aeef9c10cf6478feed4a81f4ce7a2ba94cf9708e
Jayshri-Mahale <120714712+Jayshri-Mahale@users.noreply.github.com>
2026-04-20 12:01:20 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "ap-south-1" 
+       default     = "us-east-1" 
 }
 
 variable "key_name" { 
